cb57a3d5b0032db82a7c3d4004077ebc764e2e70
Thiago Ripardo de Lima <thiago.ripardo.86@gmail.com>
2022-10-14 17:37:19 -0400

    Adicionado dependencia Identity
    Adicionado propriedades a classe AppUser

--- 4 - Entities/Entities/Entities/ApplicationUser.cs
+++ 4 - Entities/Entities/Entities/ApplicationUser.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace Entities.Entities
 {
     public class ApplicationUser : IdentityUser
     {
-        
+        [Column("USR_CPF")]
+        public string CPF { get; set; }
+
+        [Column("USR_TIPO")]
+        public int Tipo { get; set; }
     }
 }
