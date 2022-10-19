7e4be74e3f25006b963d152dff1da99197ed0caf
Thiago Ripardo de Lima <thiago.ripardo.86@gmail.com>
2022-10-19 17:32:41 -0400

    Configaração jwt

---
+++ WebApis/Models/Login.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApis.Models
+{
+    public class Login
+    {
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string cpf { get; set; }
+
+    }
+}
---
+++ WebApis/Models/MessageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApis.Models
+{
+    public class MessageViewModel
+    {
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Titulo { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public string UserId { get; set; }
+    }
+}
